9a859fcdbfa1015eedcf3f3e449aba4bf0b695fe
Owen Moogk <owenmoogk@gmail.com>
2023-10-28 14:41:38 -0400

    shabibi gupipip

--- main.py
+++ main.py
@@ -21,32 +21,32 @@ from matplotlib import pyplot as plt
 import cv2
 
 def houghCircleDetector(img, og_image):
-    og_image = cv2.imread(og_image, 0)
     
     img = cv2.medianBlur(img,3)
     img_edge = cv2.Canny(img,50,100)
-    plt.figure(figsize=(20,10))
-    #Note: matplotlib uses RGB format so had to convert BGR-to-RGB
-    plt.imshow(cv2.cvtColor(img_edge,cv2.COLOR_BGR2RGB))
-    plt.title('RGB Image',color='c')
-    plt.show()
+    # plt.figure(figsize=(20,10))
+    # #Note: matplotlib uses RGB format so had to convert BGR-to-RGB
+    # plt.imshow(cv2.cvtColor(img_edge,cv2.COLOR_BGR2RGB))
+    # plt.title('RGB Image',color='c')
+    # plt.show()
 
     # circles = cv2.HoughCircles(img_edge,cv2.HOUGH_GRADIENT,1,minDist=20,param1=200,param2=70)
     circles = cv2.HoughCircles(img_edge, cv2.HOUGH_GRADIENT, dp=1.3, minDist=20, param1=100, param2=45, minRadius=0, maxRadius=50)
-    circles = np.uint16(np.around(circles))
-    print(circles)
-    for val in circles[0,:]:
-        cv2.circle(og_image,(val[0],val[1]),val[2],(255,0,0),2)
+    try:
+      circles = np.uint16(np.around(circles))
+      for val in circles[0,:]:
+          cv2.circle(og_image,(val[0],val[1]),val[2],(255,0,0),2)
+    except:
+       pass
 
-    plt.figure(figsize=(20,10))
-    plt.subplot(121),plt.imshow(cv2.cvtColor(img_edge,cv2.COLOR_BGR2RGB)),plt.title('Input',color='c')
-    plt.subplot(122),plt.imshow(cv2.cvtColor(og_image,cv2.COLOR_BGR2RGB)),plt.title('Result',color='c')
-    plt.show()
+    # plt.figure(figsize=(20,10))
+    # plt.subplot(121),plt.imshow(cv2.cvtColor(img_edge,cv2.COLOR_BGR2RGB)),plt.title('Input',color='c')
+    # plt.subplot(122),plt.imshow(cv2.cvtColor(og_image,cv2.COLOR_BGR2RGB)),plt.title('Result',color='c')
+    # plt.show()
 
 #Shape Detection
 # path_to_img = 'Metal_6.png'
-def detectShapes(img_path):
-    img = cv2.imread(img_path,0)
+def detectShapes(img):
     img = cv2.medianBlur(img,3)
     img_edge = cv2.Canny(img,50,100)
     # plt.figure(figsize=(20,10))
@@ -71,10 +71,30 @@ def detectShapes(img_path):
     # plt.figure(figsize=(200,100))
     # plt.subplot(131),plt.imshow(cv2.cvtColor(img_edge,cv2.COLOR_BGR2RGB)),plt.title('Input',color='c')
     # plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB)),plt.title('Result')
-    # # plt.subplot(133),plt.imshow(cv2.cvtColor(cv2.imread(img_path),cv2.COLOR_BGR2RGB)),plt.title('Original')
     # plt.show()
     return blank
 
-og_imge = "Metal_6.jpg"
-outline = detectShapes(og_imge)
-houghCircleDetector(outline, og_imge)
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    outline = detectShapes(image)
+    houghCircleDetector(outline, frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
